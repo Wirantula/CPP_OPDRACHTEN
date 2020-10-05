02a23614d363d5551e112fb55f4581feadb9d1c1
Wirantula <joshuawiranta@hotmail.com>
2020-10-05 13:41:50 +0200

    fix pointer with no delete

--- Les2/Opdracht3/Opdracht3/Opdracht3.cpp
+++ Les2/Opdracht3/Opdracht3/Opdracht3.cpp
@@ -6,14 +6,14 @@
 
 int main()
 {
-	Docent* docent = new Docent(1805.95f, 30, "Edwin");
-	Student* student = new Student(3024422, 20, "Joshua");
-	std::cout << "Student Nummer: " << student->getStudentNr() << std::endl;
-	std::cout << "Student Naam: " << student->getName() << std::endl;
-	std::cout << "Student Leeftijd: " << student->getAge() << std::endl;
-	std::cout << "Docent Naam: " << docent->getName() << std::endl;
-	std::cout << "Docent Leeftijd: " << docent->getAge() << " Wat Edwin zijn echte leeftijd is zal altijd een mysterie blijven" << std::endl;
-	std::cout << "Docent Maand Salaris: " << docent->getSalaris() << " Ook zal Edwin liever een hoger salaris willen" << std::endl;
+	Docent docent = Docent(1805.95f, 30, "Edwin");
+	Student student = Student(3024422, 20, "Joshua");
+	std::cout << "Student Nummer: " << student.getStudentNr() << std::endl;
+	std::cout << "Student Naam: " << student.getName() << std::endl;
+	std::cout << "Student Leeftijd: " << student.getAge() << std::endl;
+	std::cout << "Docent Naam: " << docent.getName() << std::endl;
+	std::cout << "Docent Leeftijd: " << docent.getAge() << " Wat Edwin zijn echte leeftijd is zal altijd een mysterie blijven" << std::endl;
+	std::cout << "Docent Maand Salaris: " << docent.getSalaris() << " Ook zal Edwin liever een hoger salaris willen" << std::endl;
 	return 0;
 }
 
